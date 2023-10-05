1be1ef013486c35e022a9b98fb18b58c579d72db
Mateus Arruda Navarro Albuquerque <mateusnavarro@2ndmarket.com.br>
2023-10-05 15:25:49 -0300

    upgraded code

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,20 @@ import fs from 'fs';
  * @param {string} network The network name
  * @param {string} path The path you want to save
  */
-function saveDeployment(contractAddress="No address provided",network="No provided",path="./deployment-history.txt"){
-    const divisor = "##############################################################################\n"
-    const deploymentString = divisor+`Deployed: ${contractAddress} Network:${network} in ${new Date().toString()} \n` +divisor 
-    fs.appendFileSync(path,deploymentString,{encoding:"utf-8"})
-    console.log("Deployment saved")
+function saveDeployment(
+	contractAddress = 'No address provided',
+	network = 'No provided',
+	path = './artifacts/deployment-history.txt'
+) {
+	const divisor =
+		'##############################################################################\n';
+	const deploymentString =
+		divisor +
+		`Deployed: ${contractAddress}\n Network:${network}\n Date: ${new Date().toString()} \n` +
+		divisor;
+	fs.appendFileSync(path, deploymentString, { encoding: 'utf-8' });
+	console.log(`Deployment saved in ${path}`);
 }
-
 export {
     saveDeployment
 }
